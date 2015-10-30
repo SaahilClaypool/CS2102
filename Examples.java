ea55fd1fc9e4fbdb1e7f67d4b50c333d0ad0cd4f
SaahilClaypool <saahil@claypools.org>
2015-10-29 20:31:20 -0400

    first hw and examples class

---
+++ Examples.java
@@ -0,0 +1,37 @@
+import tester.* ;
+class Examples {
+    Examples () {} ;
+
+  // example data
+
+    // checkFuntion
+  /*
+  * public boolean testFun (Tester t)
+  * {
+  * return t.checkExpect (fun , true/false);
+  *
+  * }
+  *
+  *
+  *
+  * */
+
+    /**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * TO RUN TESTS:::
+     *
+     *
+     *
+     *
+     *
+     static Examples E = new Examples () ;
+
+     public static void main(String[] args) {
+     Tester.run (E) ; / Tester.runFullReport(E);
+
+
+
+     }
+     }
+     *
+     */
+}
